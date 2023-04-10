64c9a6cd6ccc1dbc6dfc44863c2c1dc860f8c65c
David Perelman <dperelman3@gmail.com>
2023-04-10 19:47:04 +0300

    Navbar

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,8 +1,41 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
 const Navbar = () => {
+  const [loggedin, setLoggedin] = useState(false);
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+      {loggedin && (
+        <div className='container-fluid'>
+          <Link className='navbar-brand' href='/'>
+            Dashboard
+          </Link>
+          <Link className='navbar-brand' href='/'>
+            Products
+          </Link>
+          <Link className='navbar-brand' href='/'>
+            Users
+          </Link>
+          <Link className='navbar-brand' href='/'>
+            Orders
+          </Link>
+          <Link className='navbar-brand' href='/'>
+            Logout
+          </Link>
+        </div>
+      )}
+      {!loggedin && (
+        <div className='container-fluid'>
+          <Link className='navbar-brand' href='/'>
+            Dashboard
+          </Link>
+          <Link className='navbar-brand' href='/'>
+            Login
+          </Link>
+        </div>
+      )}
+    </nav>
   );
 };
 
