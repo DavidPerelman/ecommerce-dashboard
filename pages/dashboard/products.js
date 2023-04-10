4ebf20c7ca6cdb7905512330693145d11c5c5f3e
David Perelman <dperelman3@gmail.com>
2023-04-10 22:19:14 +0300

    add images and actions to products table

--- pages/dashboard/products.js
+++ pages/dashboard/products.js
@@ -1,20 +1,24 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import DataTable from 'react-data-table-component';
+import styles from './products.modules.css';
 
-const Products = () => {
+const AllProductsList = () => {
   const [products, setProducts] = useState([
     {
+      id: 'p1',
       title: 'Samsung Galaxy A52',
       category: 'Phones',
       price: 500,
-      image: '/images/sa-en-galaxy-a52-a525-sm-a525fzkhmea-475124311.png',
+      image:
+        '/images/products/phones/sa-en-galaxy-a52-a525-sm-a525fzkhmea-475124311.png',
     },
     {
+      id: 'p2',
       title: 'AirPods Pro (2nd generation)',
       category: 'AirPods',
       price: 249,
-      image: '/images/MQD83.jpg',
+      image: '/images/products/airPods/MQD83.jpg',
     },
   ]);
 
@@ -40,14 +44,24 @@ const Products = () => {
       selector: (row) => row.price,
       sortable: true,
     },
+    {
+      name: 'Action',
+      selector: (row) => (
+        <div>
+          <button onClick={() => console.log(row.id)}>delete</button>
+          <button onClick={() => console.log(row.id)}>edit</button>
+        </div>
+      ),
+      sortable: true,
+    },
   ];
 
   return (
-    <div>
-      <h1>Products</h1>
+    <div className={styles.AllProductsList}>
+      <button className='btn btn-primary'>Add new product</button>
       <DataTable columns={columns} data={products} />
     </div>
   );
 };
 
-export default Products;
+export default AllProductsList;
